34c739697dfedc6ab455cb52e918f919752ab52e
Felix Ribeiro <ribeiro.felix@live.com>
2013-09-15 18:52:28 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		if(args.length < 3){
+			System.out.println("Numero de parametros invalidos.");
+			System.out.println("Parametros esperados : Tipo_Digest Caminho_ArqListaDigest Caminho_Arq1... Caminho_ArqN");
+			System.exit(1);
+		}
+		
+		
+
+	}
+
+}
