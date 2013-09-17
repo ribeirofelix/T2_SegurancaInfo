67c8bbba1095f29cbe3fae623f14df46557e4c24
Fábio Mourthé <fabio.clemente@vtex.com.br>
2013-09-16 23:20:24 -0300

    FileManager v2

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,12 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class FileManager {
@@ -23,7 +28,52 @@ public class FileManager {
 		return this.map != null ? this.map : this.GenerateMapByteFile();		
 	}
 	
-	public static Map<String, HashMap<String, String>> getDisgestMap(String filePath){
+	public static void UpdateFileListDigest(String pathToListDigestFile, Map<String, String> map){
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(pathToListDigestFile));
+			List<String> lines = new ArrayList<String>();
+			
+			String fileLine;
+			while ((fileLine = reader.readLine()) != null){
+				
+				lines.add(fileLine);				
+			}
+			
+			reader.close();			
+			
+			List<String> fileContent = new ArrayList<String>();
+			for (String line : lines) {
+				
+				String[] args = line.split(" ");
+				if(map.containsKey(args[0])){
+					
+				    fileContent.add(line.concat(" " + map.get(args[0])));
+				}
+				else{
+					fileContent.add(line);
+				}
+				
+				map.keySet().remove(args[0]);
+			}			
+			
+			for (String key : map.keySet()) {
+				fileContent.add(key + " " + map.get(key));
+			}
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(pathToListDigestFile, false));
+			for (String line : fileContent) {
+				writer.write(line + System.getProperty("line.separator"));
+			}
+			
+			writer.close();			
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static Map<String, HashMap<String, String>> getDigestMap(String filePath){
 	
 		try{
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -54,6 +104,9 @@ public class FileManager {
 		}
 		catch (IOException e){
 			e.printStackTrace();
+			System.out.println("");
+			System.exit(1);
+			
 			return null;
 		}
 	}
@@ -62,7 +115,7 @@ public class FileManager {
 		this.map = new HashMap<String, byte[]>();
 		
 		for (String path : this.paths){
-			this.map.put(path, this.readFile(path));
+			this.map.put(GetNameFromPath(path), this.readFile(path));
 		}
 		
 		return this.map;
@@ -74,6 +127,12 @@ public class FileManager {
 		byte[] fileBytes = getBytes(file);
 		
 		return fileBytes;
+	}
+	
+	private static String GetNameFromPath(String path){
+		int lastSlashIndex = path.replaceAll("\\", "/").lastIndexOf("/");
+		
+		return lastSlashIndex >= 0 ? path : path.substring(lastSlashIndex + 1);
 	}
 	
 	private byte[] getBytes (File file){
